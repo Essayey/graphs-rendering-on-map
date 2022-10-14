aeb743aac1036073958524e394876df2e745a826
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-14 17:08:22 +0300

    Apply DG.geoJson to render graph

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 var map;
+let paths;
+let nodes;
+let sources;
+let destinations;
 
-const graph = {
-    vertices: [
-        { id: 0, x: 54.515724, y: 36.245781 }, // 54.515724° 36.245781°
-        { id: 1, x: 54.515507, y: 36.247707 }, // 54.515507° 36.247707°
-        { id: 2, x: 54.517602, y: 36.248699 }, // 54.517602° 36.248699°
-        { id: 3, x: 54.517255, y: 36.251785 }, // 54.517255° 36.251785°
+fetch('./Layers/Paths.geojson')
+    .then(response => response.json())
+    .then(json => paths = json);
 
-        { id: 4, x: 54.51688, y: 36.254421 }, // 54.51688° 36.254421°
-        { id: 5, x: 54.514741, y: 36.253492 }, // 54.514741° 36.253492°
-        { id: 6, x: 54.514494, y: 36.255821 }, // 54.514494° 36.255821°
-        { id: 7, x: 54.51383, y: 36.261302 }, // 54.51383° 36.261302°
-        { id: 8, x: 54.512215, y: 36.26038 }, // 54.512215° 36.26038°
-    ],
-    edges: [{ vertexId1: 0, vertexId2: 1 },
-    { vertexId1: 1, vertexId2: 2 },
-    { vertexId1: 2, vertexId2: 3 },
-    { vertexId1: 1, vertexId2: 3 },
+fetch('./Layers/Nodes.geojson')
+    .then(response => response.json())
+    .then(json => nodes = json);
 
-    { vertexId1: 3, vertexId2: 4 },
-    { vertexId1: 1, vertexId2: 5 },
+fetch('./Layers/Sources.geojson')
+    .then(response => response.json())
+    .then(json => sources = json);
 
-    { vertexId1: 4, vertexId2: 5 },
-    { vertexId1: 5, vertexId2: 6 },
-    { vertexId1: 6, vertexId2: 7 },
-    { vertexId1: 6, vertexId2: 8 }],
-}
-
-// TODO
-// Необходимо добавить поле state в узел, оно будет показывать, исправен ли данный узел или нет.
-// Затем, при отрисовке связей, отходящих от неисправного узла, выбирать цвет линии (красный - линия неисправна)
-// Также, необходимо провести декомпозицию, возможно добавить отдельные функции для отрисовки узлов и линий связи.
-
+fetch('./Layers/Destinations.geojson')
+    .then(response => response.json())
+    .then(json => destinations = json);
 
 
 DG.then(function () {
@@ -39,13 +26,30 @@ DG.then(function () {
         center: [54.514635, 36.252962],
         zoom: 16,
     });
-    graph.vertices.forEach(node => DG.circleMarker([node.x, node.y])
-        .bindPopup(`Узел водоснабжения с координатами ${node.x}, ${node.y}, уникальный идентификатор ${node.id}`)
-        .addTo(map));
-    graph.edges.forEach(edge => DG.polyline([
-        [graph.vertices[edge.vertexId1].x, [graph.vertices[edge.vertexId1].y]],
-        [graph.vertices[edge.vertexId2].x, [graph.vertices[edge.vertexId2].y]]
-    ])
-        .bindPopup(`Линия водоснабжения, связывающая узлы ${edge.vertexId1} и ${edge.vertexId2}`)
-        .addTo(map));
+
+    DG.geoJson(nodes, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('Колодец ' + feature.properties.Name + '.');
+        }
+    }).addTo(map);
+    DG.geoJson(paths, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('Труба, соединяющая участок ' + feature.properties.Begin_uch + ' и ' + feature.properties.End_uch);
+        }
+    }).addTo(map);
+
+    DG.geoJson(sources, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('Источник ' + feature.properties.Name + '.');
+        }
+
+    }).addTo(map);
+
+    DG.geoJson(destinations, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('Заход ' + feature.properties.Name + '.');
+        }
+
+    }).addTo(map);
 });
+
